8e8fc73a5e9807a7f9645e35601b9b035db27b69
Anuj <panchalanuj411@gmail.com>
2023-04-25 17:01:10 +0530

    Add sender model

--- email_book/ebook/models.py
+++ email_book/ebook/models.py
@@ -35,7 +35,7 @@ STATE_CHOICES = (
     ('Tripura','Tripura'),
     ('Uttarakhand','Uttarakhand'),
     ('Uttar Pradesh','Uttar Pradesh'),
-    ('West Bengal','West Bengal'),
+    ('West Bengal','West Bengal')
 )
 
 COUNTRY_CHOICES = (
@@ -43,7 +43,7 @@ COUNTRY_CHOICES = (
 )
 
 class sender(models.Model):
-    id = models.IntegerField(blank=False, null=False)
+    id = models.IntegerField(primary_key=True, blank=False, null=False)
     name = models.CharField(max_length=50,blank=True,null=True)
     from_email = models.EmailField(max_length=254,blank=False,null=False)
     to_email = models.EmailField(max_length=254,blank=False,null=False)
@@ -51,8 +51,8 @@ class sender(models.Model):
     company_address = models.CharField(max_length=100,blank=False,null=False)
     city = models.CharField(max_length=50,blank=False,null=False)
     state = models.CharField(max_length=50,blank=False,null=False)
-    zip_code = models.IntegerField(max_length=10,blank=False,null=False)
-    country = models.CharField(max_length=10,blank=False,null=False)
+    zip_code = models.IntegerField(blank=False, null=False)
+    country = models.CharField(max_length=10, blank=False)
 
 # class (models.Model):
 #     id = models.IntegerField(blank=False, null=False)
